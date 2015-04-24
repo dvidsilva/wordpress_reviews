2a8e8189bcfa69080034f247a3d8a610b74b1bd4
dvid <contacto@dvidsilva.com>
2015-04-24 13:07:21 -0700

    showing reviews with the shortcode

--- public/index.php
+++ public/index.php
@@ -1,68 +1,59 @@
-<link rel="stylesheet" href="dodreviews.css">
-<!-- dod reviews template for the homepage-->
+<?php $args = array(
+	'posts_per_page'   => 15,
+	'offset'           => 0,
+	'category'         => '',
+	'category_name'    => '',
+	'orderby'          => 'post_date',
+	'order'            => 'DESC',
+	'include'          => '',
+	'exclude'          => '',
+	'meta_key'         => '',
+	'meta_value'       => '',
+	'post_type'        => 'dodreview',
+	'post_mime_type'   => '',
+	'post_parent'      => '',
+	'post_status'      => 'publish',
+	'suppress_filters' => true 
+);
+// $reviews_array = get_posts( $args );
+print_r($reviews_array);
+?>
+
 <!-- main container -->
 <div id="dodreviews">
   <h1>Hear From Our Patients</h1>
   <div class="dodrslider">
+  <?php
+  foreach ($reviews_array as $review) {
+   ?>
     <!-- slide begin -->
     <div class="dodrslide">
      <div class="dodrtop">
         <div class="dodrstars">
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
+          <?php $stars =  get_post_meta($review->ID, 'stars', true);
+            for ($i = 0; $i < $stars; $i++) {
+          ?>
+            <span class="dodrstar"></span>
+          <?php
+            }
+          ?>
         </div>
       </div>
       <div class="dodrtext">
         <p class="dodrreview">
-          It was a whole lot better than waiting in an office..they were very patient answered all my questions.
-          <span class="dodrusername">&ndash;Reggie Steele</span>
+          <?php echo $review->post_content ?>.
+          <span class="dodrusername">&ndash;<?php echo get_post_meta($review->ID, 'author', true); ?></span>
         </p>
       </div>
-      <p class="dodrdate">April 2, 2015</p>
+      <p class="dodrdate"><?php echo get_post_meta($review->ID, 'date', true); ?></p>
     </div>
     <!-- slide end -->
-        <!-- slide begin -->
-    <div class="dodrslide">
-     <div class="dodrtop">
-        <div class="dodrstars">
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-        </div>
-      </div>
-      <div class="dodrtext">
-        <p class="dodrreview">
-          It was a whole lot better than waiting in an office..they were very patient answered all my questions.
-          <span class="dodrusername">&ndash;Reggie Steele</span>
-        </p>
-      </div>
-      <p class="dodrdate">April 2, 2015</p>
-    </div>
-    <!-- slide end -->
-        <!-- slide begin -->
-    <div class="dodrslide">
-     <div class="dodrtop">
-        <div class="dodrstars">
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-          <span class="dodrstar"></span>
-        </div>
-      </div>
-      <div class="dodrtext">
-        <p class="dodrreview">
-          It was a whole lot better than waiting in an office..they were very patient answered all my questions.
-          <span class="dodrusername">&ndash;Reggie Steele</span>
-        </p>
-      </div>
-      <p class="dodrdate">April 2, 2015</p>
-    </div>
-    <!-- slide end -->
+    <?php
+    }
+    ?>
   </div>
 </div>
+<!-- /main container -->
+<?php 
+  //@TODO add doctor_name and doctor_page 
+?>
