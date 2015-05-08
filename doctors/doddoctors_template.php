65939959dc9a2a5ff41340e67ea50f454ba65323
dvid <contacto@dvidsilva.com>
2015-05-08 16:16:54 -0700

    css fix

--- doctors/doddoctors_template.php
+++ doctors/doddoctors_template.php
@@ -1,9 +1,11 @@
 <?php
   get_header();
-?>
-
-<?php the_content('Read more...'); ?>
-
-<?php 
+  the_content('Read more...');
   get_footer(); 
 ?>
+<script type="text/javascript">
+// being shitty and fixing layout issues with JS
+  $(document).ready(function () {
+    $('.dodrdoctorheader').wrap('<div class="dodrheaderprev">');
+  });
+</script>
