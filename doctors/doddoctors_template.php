a29c05b491de5a0cbee404a4840a3ce4313b3c09
dvid <contacto@dvidsilva.com>
2015-05-13 21:53:13 -0700

    removed herar from our patients

--- doctors/doddoctors_template.php
+++ doctors/doddoctors_template.php
@@ -8,10 +8,20 @@
 </div>
 <?php  
   get_footer(); 
+  
+  $doctor_type = get_post_meta(get_the_ID(), 'doctor_type', true);
+
 ?>
 <script type="text/javascript">
 // being shitty and fixing layout issues with JS
   $(document).ready(function () {
     $('.dodrdoctorheader').wrap('<div class="dodrheaderprev">');
   });
+  <?php
+    if($doctor_type==="pediatrician"){
+  ?>
+    $('.dodrdoctorheader [href*="medical"]').attr('href','http://wwwtest.doctorondemand.com/pediatrics');
+  <?php
+    }
+  ?>
 </script>
